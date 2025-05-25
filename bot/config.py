48192b92660574b5cb3dadc37ddcff6cd9133ad9
twise238 <83116995+twise238@users.noreply.github.com>
2025-05-26 01:15:05 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,7 +1,7 @@
-﻿BOT_TOKEN = "7775459935:AAE6TqDemqSzIso1wLXjzopiTxTBP1FBL0Y"  # Токен бота
-CHANNEL_URL = "https://t.me/+PcPPhEhQUk81MzQy"  # Ссылка на канал (проверка сабки)
-CHANNEL_ID = "-1002447416539"  # ID канала. Начинается с минуса (проверка сабки)
-VERIF_CHANNEL_ID = "-1002270566537"  # ID обсуждений канала для верификации айдишников, начинается с минуса
+﻿BOT_TOKEN = "7775459935:###6TqDemqSzIso1wLXjzopiTxTBP1FBL0Y"  # Токен бота
+CHANNEL_URL = "https://t.me/+###PhEhQUk81MzQy"  # Ссылка на канал (проверка сабки)
+CHANNEL_ID = "-###"  # ID канала. Начинается с минуса (проверка сабки)
+VERIF_CHANNEL_ID = "-###"  # ID обсуждений канала для верификации айдишников, начинается с минуса
 SUPP = "https://t.me/StillEggs"
 ADMIN_ID = 863218618 #свой айди
 
